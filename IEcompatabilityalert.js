746c0dd680a20da57710303868420ee9498a970c
Hans Fernandez <hf08@my.fsu.edu>
2019-07-05 12:07:24 -0400

    Create IEcompatabilityalert.js

---
+++ IEcompatabilityalert.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">	
+//Detect if IE is used
+if(navigator.appName.indexOf("Internet Explorer")!=-1 || navigator.userAgent.match(/Trident.*rv[ :]*11\./))
+{
+//Put code here that executes if the user uses IE
+alert("We've detected you are using Microsoft Internet Explorer to visit us. This browser may not display correctly. Please consider using Google Chrome or Mozilla Firefox to avoid display issues. ");
+
+}
+</script>
